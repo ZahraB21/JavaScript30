d6b02b98f3ced979d2cf31e3fb86b5d33de05916
Zahra <zbehfarshad@gmail.com>
2020-10-20 12:44:59 -0700

    Updating the progress bar when clicked on it

--- Day11/script.js
+++ Day11/script.js
@@ -29,13 +29,27 @@ function updateRange() {
   video[this.name] = this.value;
 }
 
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+function updateProgressBar() {
+  const updatedProgressBar = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${updatedProgressBar}%`;
+}
+
 //  Hook up the event listeners
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", updateProgressBar);
 toggle.addEventListener("click", togglePlay);
 
 skipButtons.forEach((button) => button.addEventListener("click", updateVideo));
 
 ranges.forEach((range) => range.addEventListener("change", updateRange));
 ranges.forEach((range) => range.addEventListener("mousemove", updateRange));
+
+progress.addEventListener("click", scrub);
+// progress.addEventListener("mousemove", scrub);

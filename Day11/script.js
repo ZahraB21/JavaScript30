add78c4c381b79b10ae5ee8ffb7a3e385b2f62cd
Zahra <zbehfarshad@gmail.com>
2020-10-20 12:20:28 -0700

    Adding the skip and play/pause button features

--- Day11/script.js
+++ Day11/script.js
@@ -5,7 +5,7 @@ const progress = player.querySelector(".progress");
 const progressBar = player.querySelector(".progress_filled");
 const toggle = player.querySelector(".toggle");
 const ranges = player.querySelectorAll(".player_slider");
-const skipButtons = player.querySelectorAll("[data-slip]");
+const skipButtons = player.querySelectorAll("[data-skip]");
 
 // Build the functions
 function togglePlay() {
@@ -15,5 +15,20 @@ function togglePlay() {
     video.pause();
   }
 }
+
+function updateButton() {
+  const icon = this.paused ? "►" : "❚ ❚";
+  toggle.textContent = icon;
+}
+
+function updateVideo() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
 //  Hook up the event listeners
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
 toggle.addEventListener("click", togglePlay);
+
+skipButtons.forEach((button) => button.addEventListener("click", updateVideo));

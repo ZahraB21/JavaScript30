5073beaca82e6f108b9aa9a20c6e383ac90e893b
Zahra <zbehfarshad@gmail.com>
2020-10-25 17:40:18 -0700

    Listing the added items

--- Day15/script.js
+++ Day15/script.js
@@ -3,4 +3,23 @@ const itemsList = document.querySelector('.plates');
 
 const items = [];
 
-alert('hello');
+function addItem(e){
+    e.preventDefault();
+    const text = (this.querySelector('input[type=text]')).value; // this, refers to the form
+    const item = {
+        text,
+        done: false
+    }
+    items.push(item);
+    itemsList.innerHTML = items.map( (item, index) => {
+        return `
+            <li>
+                <input type = "checkbox" data-index = ${item.index} id = ${item.text}>
+                <label for = ${item.text}>${item.text}</label>
+            </li>
+        `}).join('');
+
+    this.reset(); // resetting the input by resetting the form
+}
+
+addItems.addEventListener('submit', addItem);

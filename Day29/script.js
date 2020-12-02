11639210b1dd7699c096f88efdaf9718efee615c
Zahra <zbehfarshad@gmail.com>
2020-12-01 16:32:38 -0800

    Displaying the timer and the returning time

--- Day29/script.js
+++ Day29/script.js
@@ -1,23 +1,34 @@
 const timeLeft = document.querySelector('.display_time-left');
 const endTime = document.querySelector('.display_end-time');
+let timerStop;
 
 function timer(seconds){
+    clearInterval(timerStop);
     const now = Date.now();
     let then = now + seconds * 1000;
-    displayTimer(then);
+    displayTimer(seconds);
+    displayComeBack(then);
 
-    setInterval( () => {
-        then -= 1000;
-        if (then > 0){
-            displayTimer(then);
+    timerStop = setInterval( () => {
+        seconds--;
+        if (seconds >= 0){
+            displayTimer(seconds);
+        } else {
+            clearInterval(timerStop);
         }
     }, 1000);
 }
 
-function displayTimer(timestamp){
+function displayTimer(seconds){
+    let minutes = Math.floor(seconds / 60);
+    let remainingSeconds = seconds % 60;
+    timeLeft.textContent = `${minutes}: ${remainingSeconds >= 10 ? '' : '0'}${remainingSeconds}`;
+}
+
+function displayComeBack(timestamp){
     let time = new Date(timestamp);
-    let minutes = (new Date(Date.now())).getMinutes() - time.getMinutes();
-    let seconds = time.getSeconds();
-    timeLeft.textContent = `${minutes}:${seconds > 10 ? '' : '0'}${seconds}`;
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    endTime.textContent = `${hours}:${minutes > 10 ? '' : '0'}${minutes}`;
 }
 
